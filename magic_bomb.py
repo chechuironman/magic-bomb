02b8b6894a2738aa15419ce5b90fd7b68e270766
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2021-03-14 21:06:37 +0100

    first commit

--- magic_bomb.py
+++ magic_bomb.py
@@ -1,7 +1,7 @@
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
-# import binance.expections
-# from json2table import convert
+from binance.websockets import BinanceSocketManager
+
 import json
 
 class magic_bomb:
@@ -16,6 +16,66 @@ class magic_bomb:
             print(e.message)
     def account(self):
         account = self.client.get_account()
-        for value in account['balances']:
-            if (float(value['free']) != float(0)) or (float(value['locked']) != float(0)):
-                print(value)
+        # for value in account['balances']:
+            # if (float(value['free']) != float(0)) or (float(value['locked']) != float(0)):
+            #     print(value)
+        print(account)
+    # def get_pairs(self):
+    def get_istorical_trades(self):
+        trades = self.client.get_historical_trades(symbol='CHZBUSD')  
+        print(trades)  
+    def get_all_orders(self, pair):
+        orders = self.client.get_all_orders(symbol=pair) 
+        success_orders=[]
+        for order in orders:
+            if order['status'] != 'CANCELED':
+                success_orders.append(order)
+        return success_orders 
+    def get_balance_pair(self, pair):
+        orders = self.get_all_orders( pair)
+        BUYS=0
+        SELLS=0
+        for order in orders:
+            if order['side'] == 'SELL':
+                SELLS = SELLS + (float(order['executedQty'])*float(order['price']))
+            else:
+                if order['side'] == 'BUY':
+                    BUYS = BUYS + (float(order['executedQty'])*float(order['price']))
+                else: 
+                    print("hay mas mierda")      
+        balance = SELLS - BUYS
+        return balance
+    def get_coins(self):
+        details = self.client.get_all_tickers()
+        coins = []
+        for coin in details:
+            coins.append(coin['symbol'])
+        print(coins)
+
+    # def get_all_coins(self):
+    #     coins = self.client.get_all_coins_info()
+    #     print(coins)
+    # def margin_trades(self):
+    #     trades = self.client.get_margin_trades(symbol='CHZUSDT')
+    #     print(trades)
+
+class websocket:   
+    def __init__(self,api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        try: 
+            self.client = Client(self.api_key, self.api_secret)
+            print("Client authenticated")
+        except BinanceAPIException as e:
+            print(e.status_code)
+            print(e.message) 
+    def websocket(self):
+        bm = BinanceSocketManager(self.client)
+        # start any sockets here, i.e a trade socket
+        conn_key = bm.start_trade_socket('BNBBTC', self.process_message)
+        # then start the socket manager
+        bm.start()  
+    def process_message(self, msg):
+        print("message type: {}".format(msg['e']))
+        print(msg)
+    # do something    
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from magic_bomb import magic_bomb
+from magic_bomb import websocket
 from binance.client import Client
 api_key="AY5bm2HEn6w5OUAAhCQzt1Ifrwl5EnLGKRAJAf9ZTpLsQfc18KrAQMCxobiQYH5y"
 api_secret="cZREtIOeWVYCo3Jsa8kgMRBMiRlVsdigawMklBB5fFKkGuU04xZjFwdyFkoxWKF3"
@@ -6,4 +7,13 @@ binance = magic_bomb(api_key, api_secret)
 
 # account = binance.account()
 
-pair = binance.pairs()
+# pair = binance.pairs()
+
+# print(binance.get_istorical_trades())
+# print(binance.get_balance_pair('CHZUSDT'))
+# binance.get_coins()
+binance.account()
+
+
+# websocket = websocket(api_key, api_secret)
+# websocket = websocket.websocket()
