2ae616e201efee77f718df02748d968efc3b4cbb
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2021-03-15 21:48:50 +0100

    first commit

---
+++ get_historic_data.py
@@ -0,0 +1,25 @@
+from magic_bomb import magic_bomb
+from magic_bomb import websocket
+from binance.client import Client
+from pprint import pprint
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+api_key="AY5bm2HEn6w5OUAAhCQzt1Ifrwl5EnLGKRAJAf9ZTpLsQfc18KrAQMCxobiQYH5y"
+api_secret="cZREtIOeWVYCo3Jsa8kgMRBMiRlVsdigawMklBB5fFKkGuU04xZjFwdyFkoxWKF3"
+binance = magic_bomb(api_key, api_secret)
+
+klines = binance.get_historical_klines('CHZUSDT','5 days ago UTC')
+# pprint(klines)
+# for kline in klines['high']:
+#     print('high {}'.format(kline))
+max_klines = max(klines, key=lambda ev: ev['high'])
+min_klines = min(klines, key=lambda ev: ev['low'])
+avg_max = max(klines, key=lambda ev: ev['avg'])
+avg_min = min(klines, key=lambda ev: ev['avg'])
+pprint(klines)
+pprint(klines)
+print(avg_max)
+print(avg_min)
+print('max: {}, min: {}, avg_max: {}, avg_min: {}'.format(max_klines['high'],min_klines['low'],avg_max['avg'],avg_min['avg']))
+# plt.plot(klines['date'], klines['high'])
--- magic_bomb.py
+++ magic_bomb.py
@@ -1,7 +1,7 @@
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 from binance.websockets import BinanceSocketManager
-
+from datetime import datetime
 import json
 
 class magic_bomb:
@@ -35,35 +35,50 @@ class magic_bomb:
             if order['status'] != 'CANCELED':
                 success_orders.append(order)
         return success_orders 
-    def get_balance_pair(self, pair):
-        orders = self.get_all_orders( pair)
-        BUYS=0
-        SELLS=0
-        for order in orders:
-            if order['side'] == 'SELL':
-                SELLS = SELLS + (float(order['executedQty'])*float(order['price']))
-            else:
-                if order['side'] == 'BUY':
-                    BUYS = BUYS + (float(order['executedQty'])*float(order['price']))
-                else: 
-                    print("hay mas mierda")      
-        balance = SELLS - BUYS
-        return balance
+    def get_balance_pair(self):
+        orders = self.get_all_orders('ALL')
+        print(orders)
+        # BUYS=0
+        # SELLS=0
+        # for order in orders:
+        #     if order['side'] == 'SELL':
+        #         SELLS = SELLS + (float(order['executedQty'])*float(order['price']))
+        #     else:
+        #         if order['side'] == 'BUY':
+        #             BUYS = BUYS + (float(order['executedQty'])*float(order['price']))
+        #         else: 
+        #             print("hay mas mierda")      
+        # balance = SELLS - BUYS
+        # return balance
     def get_coins(self):
         details = self.client.get_all_tickers()
         coins = []
         for coin in details:
             coins.append(coin['symbol'])
-        print(coins)
+        return coins
     def total_balance(self):
-        pairs = self.account()
+        pairs = self.get_coins()
+        # print(pairs)
         # print(values['all_values'])
         # pairs = values['all_values']
         balances = []
         for pair in pairs:
+            print('pair: {}', pair)
             balances.append(self.get_balance_pair(pair))
         return balances
-        
+    def get_asset_dividend_histrory(self):
+        print(self.client.get_asset_dividend_history())
+    def get_my_trades(self):
+        print(self.client.get_my_trades(symbol=''))
+    def get_historical_trades(self,pair):
+        trades = self.client.get_historical_trades(symbol=pair)
+        return(trades)
+    def get_historical_klines(self,pair, period):
+        klines = self.client.get_historical_klines(pair, Client.KLINE_INTERVAL_1MINUTE, period)
+        clean_klines = []
+        for kline in klines:
+            clean_klines.append({'date':datetime.fromtimestamp(int(kline[0])/1000), 'high': kline[2], 'low': kline[3], 'avg': (float(kline[2])+float(kline[3]))/2})
+        return clean_klines
     # def get_all_coins(self):
     #     coins = self.client.get_all_coins_info()
     #     print(coins)
