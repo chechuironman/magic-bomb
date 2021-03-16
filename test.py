69084097a5504e1b603bae2a5904d37b22cc8603
Chechu <chechuironman@gmail.com>
2021-03-16 23:37:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from magic_bomb import magic_bomb
 from magic_bomb import websocket
 from binance.client import Client
-api_key="AY5bm2HEn6w5OUAAhCQzt1Ifrwl5EnLGKRAJAf9ZTpLsQfc18KrAQMCxobiQYH5y"
-api_secret="cZREtIOeWVYCo3Jsa8kgMRBMiRlVsdigawMklBB5fFKkGuU04xZjFwdyFkoxWKF3"
+api_key=""
+api_secret=""
 binance = magic_bomb(api_key, api_secret)
 
 # account = binance.account()
